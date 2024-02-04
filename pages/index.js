9da695c402331563dc6dbd8c9ca5c88575b8677f
alexsaysdie <158591481+alexsaysdie@users.noreply.github.com>
2024-02-03 21:17:48 -0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -21,3 +21,51 @@ export default function Home() {
     </div>
   )
 }
+
+
+
+
+<body>
+    <div class="navbar">
+        <h1></h1>
+        <nav>
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="about-update.html">About Update</a></li>
+                <li><a href="Bugs-report.html">Report Issue</a></li>
+            </ul>
+        </nav>
+    </div>
+    <div class="logo-container">
+        <div class="logo">
+            <img src="Images/enlarged logo.png" alt="Logo">
+        </div>
+    </div>
+    <div class="big-title">
+       Infmapi is back as <span class="purple-gradient">INFMAPX</span>
+    </div>
+    <p class="small-text">Still alpha version adding games by the day</p>
+    <center>
+        <button class="glow-on-hover" type="button" id="playButton">Play Now!</button>
+    </center>
+    <div class="line"></div>
+    <script>
+      const playButton = document.querySelector('#playButton');
+    const iframe = document.createElement('iframe');
+    iframe.style.width = '100%';
+    iframe.style.height = '100%';
+    iframe.style.position = 'absolute';
+    iframe.style.top = '0';
+    iframe.style.left = '0';
+    iframe.style.bottom = '0';
+    iframe.style.right = '0';
+
+    playButton.addEventListener('click', () => {
+        const newWindow = window.open('about:blank', '_blank');
+        iframe.src = 'https://infmapi.netlify.app/selection.html';
+        newWindow.document.body.appendChild(iframe);
+    });
+    </script>
+
+
+</body>
